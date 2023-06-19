8ec364fe68ca22983194b5e79752a7c58f0b5051
LucasRigon <rrigon1004@gmail.com>
2023-06-19 15:59:27 -0300

    Versão 6

---
+++ RPG Versão 6/RPG/src/Itens.java
@@ -0,0 +1,16 @@
+
+ class Itens {
+	 
+	 private Itens ultraMarine;
+	 public void ligue (Marine Itens) {
+		 ultraMarine = bolter;
+		 	
+	 }
+
+	 Itens bolter = new Itens("Bolter", 5);
+	 
+	 public Itens(String name, int power) {
+			
+		}
+	 
+}
